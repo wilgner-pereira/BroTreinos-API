829ba2eb9811d24b13b4076f928267c5d7585927
Wilgner <wilgnerpr.dev@gmail.com>
2025-08-18 21:26:22 -0300

    Adicionado teste para UserserviceImpl

--- src/main/java/br/com/wilgner/brotreinos/Calculadora.java
+++
@@ -1,7 +0,0 @@
-package br.com.wilgner.brotreinos;
-
-public class Calculadora {
-    public int somar(int a, int b) {
-        return a + b;
-    }
-}
---
+++ src/test/java/br/com/wilgner/brotreinos/UserServiceImplTest.java
@@ -0,0 +1,75 @@
+package br.com.wilgner.brotreinos;
+
+import br.com.wilgner.brotreinos.exception.BusinessRuleException;
+import br.com.wilgner.brotreinos.exception.ErrorCode;
+import br.com.wilgner.brotreinos.model.dto.userdto.UserRegisterDTO;
+import br.com.wilgner.brotreinos.model.dto.userdto.UserResponseDTO;
+import br.com.wilgner.brotreinos.model.entities.User;
+import br.com.wilgner.brotreinos.model.repository.UserRepository;
+import br.com.wilgner.brotreinos.service.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @InjectMocks
+    private UserServiceImpl userServiceImpl;
+
+    private UserRegisterDTO userDto;
+    private User userEntity;
+
+    @BeforeEach
+    void setUp() {
+    userDto = new UserRegisterDTO("wilgn", "123456");
+    userEntity = new User();
+    userEntity.setId(1L);
+    userEntity.setUsername(userDto.username());
+    userEntity.setPassword("hashedPassword");
+    }
+
+    @Test
+    void createUser_whenUsernameNotExists_thenReturnResponseDTO(){
+        when(userRepository.findByusername(userDto.username())).thenReturn(Optional.empty());
+        when(bCryptPasswordEncoder.encode(userDto.password())).thenReturn("hashedPassword");
+        when(userRepository.save(any(User.class))).thenReturn(userEntity);
+
+        UserResponseDTO result = userServiceImpl.createUser(userDto);
+
+        assertNotNull(result);
+        assertEquals(userDto.username(), result.username());
+
+        verify(userRepository).findByusername(userDto.username());
+        verify(bCryptPasswordEncoder).encode(userDto.password());
+        verify(userRepository).save(any(User.class));
+    }
+
+    @Test
+    void createUser_whenUsernameExists_thenThrowBusinessRuleException() {
+        when(userRepository.findByusername(userDto.username())).thenReturn(Optional.of(userEntity));
+
+        BusinessRuleException thrown = assertThrows(BusinessRuleException.class, () -> {
+            userServiceImpl.createUser(userDto);
+        });
+
+        assertEquals(ErrorCode.USER_ALREADY_EXISTS, thrown.getErrorCode());
+
+        verify(userRepository).findByusername(userDto.username());
+        verifyNoMoreInteractions(userRepository, bCryptPasswordEncoder);
+    }
+}
